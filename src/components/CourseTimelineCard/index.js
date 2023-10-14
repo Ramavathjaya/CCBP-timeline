4d8d5679fe8e0481ae5bd9c9e723b9d3bf37cee5
Ramavathjaya <rathodjaya109@gmail.com>
2023-10-14 08:12:57 +0000

    solution

---
+++ src/components/CourseTimelineCard/index.js
@@ -0,0 +1,38 @@
+// Write your code here
+import {AiFillCloseCircle} from 'react-icons/ai'
+
+import {
+  CourseTitleAndDurationContainer,
+  CourseTitle,
+  DurationContainer,
+  Duration,
+  CourseDescription,
+  CourseTagsList,
+  CourseTagItem,
+  CourseTag,
+} from './styledComponents'
+
+const CourseTimelineCard = props => {
+  const {courseDetails} = props
+  const {courseTitle, description, duration, tagsList} = courseDetails
+
+  return (
+    <>
+      <CourseTitleAndDurationContainer>
+        <CourseTitle>{courseTitle}</CourseTitle>
+        <DurationContainer>
+          <AiFillCloseCircle color="#171f46" />
+          <Duration>{duration}</Duration>
+        </DurationContainer>
+      </CourseTitleAndDurationContainer>
+      <CourseDescription>{description}</CourseDescription>
+      <CourseTagsList>
+        {tagsList.map(eachTag => (
+          <CourseTag>{eachTag.name}</CourseTag>
+        ))}
+      </CourseTagsList>
+    </>
+  )
+}
+
+export default CourseTimelineCard
